7dc7534d32a43b68e47c1e1a565340885102a810
andrifeldmann <andri.feldmann@bluewin.ch>
2018-12-27 00:47:56 +0100

    initial

---
+++ src/main/java/ch/kosta/pbw/polyballwerkstatt/PolyballwerkstattApplication.java
@@ -0,0 +1,14 @@
+package ch.kosta.pbw.polyballwerkstatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyballwerkstattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyballwerkstattApplication.class, args);
+	}
+
+}
+
